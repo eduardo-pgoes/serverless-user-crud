56ad21dc047b344ebcbaef433686ba8e3aae1fb4
Eduardo Paulo <eduardo.paulo@vizworx.com>
2023-01-17 18:11:07 -0300

    fix: fixing .gitignore again

--- src/services/users.service.ts
+++ src/services/users.service.ts
@@ -1,4 +1,17 @@
 import User from '../models/users.model';
+import * as dotenv from 'dotenv'
+
+dotenv.config();
+var AWS = require('aws-sdk');
+
+new AWS.DynamoDB.DocumentClient({
+    region: 'localhost',
+    endpoint: 'http://localhost:8000',
+    accessKeyId: process.env.ACCESS_KEY_ID,
+    secretAccessKey: process.env.SECRET_ACCESS_KEY_ID
+})
+
+console.log(process.env.ACCESS_KEY_ID);
 
 /*
     setting up a db schema/instance or configuring seems worth the hassle, I'll develop this service later...
