19259059297c58ecdb725a3fb339f0933903e64a
Eduardo Paulo <eduardo.paulo@vizworx.com>
2023-01-18 13:33:19 -0300

    feat(db): config knex

---
+++ src/db/knexfile.js
@@ -0,0 +1,25 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+
+  development: {
+    client: 'postgresql',
+    connection: {
+      database: 'user-crud',
+      user:     'users-crud',
+      password: 'users-crud',
+      port: 5432,
+      host: 'localhost'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+};
---
+++ src/db/migrations/20230118162528_create_users_table.js
@@ -0,0 +1,22 @@
+const usersTableName = "users";
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema
+  .createTable('users', (table) => {
+    table.increments('id');
+    table.string("user", 255).notNullable();
+    table.string("password", 255).notNullable();
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTable("users");
+};
